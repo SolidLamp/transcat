6e85463df574d911429f792357ff946f07c667ed
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2026-06-30 21:13:45 +0100

    Make more modular

--- src/flags.zig
+++ src/flags.zig
@@ -1,17 +1,15 @@
 const std = @import("std");
+const trans = @import("flags/trans.zon");
 
-//const c = @cImport({
-//    @cInclude("flags.h");
-//});
 const c = @import("flags.h");
 
 pub export fn get_flag(name: [*:0]const u8, len: usize) c.FLAG {
     const slice = name[0..(len - 1)];
     var array: [5]u32 = [5]u32{ 0, 0, 0, 0, 0 };
-    
+
     // We cannot use switch here because it is a string.
     if (std.mem.eql(u8, slice, "trans") == true) {
-        array = [5]u32{ 0x5BCEFA00, 0xF5A9B800, 0xffffff00, 0xF5A9B800, 0x5BCEFA00 };
+        array = trans.colours;
     } else {
         array = [5]u32{ 0, 0, 0, 0, 0 };
     }
